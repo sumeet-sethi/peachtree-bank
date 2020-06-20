ddf6c90ab50dcb285e316f77b397b7e78f3f85fc
Sumeet Sethi <sumeetsethi.asm@gmail.com>
2020-06-20 18:34:04 -0400

    Adding components and routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { TransferComponent } from './transfer/transfer.component';
+import { TransactionsComponent } from './transactions/transactions.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'transfer', component: TransferComponent },
+  { path: 'transactions', component: TransactionsComponent },
+  { path: '404', component: NotFoundComponent },
+  { path: '', redirectTo: '/transfer', pathMatch: 'full' },
+  { path: '**', component: NotFoundComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,29 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatCardModule } from '@angular/material/card';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+
+
+import { TransferComponent } from './transfer/transfer.component';
+import { TransactionsComponent } from './transactions/transactions.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TransferComponent,
+    TransactionsComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatToolbarModule
+    MatToolbarModule, MatCardModule, MatDividerModule, MatButtonModule, MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
