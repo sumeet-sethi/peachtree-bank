bfe0f26c94fd43f7c4c76df7d2010a0607dee448
Sumeet Sethi <sumeetsethi.asm@gmail.com>
2020-11-13 23:13:30 -0500

    e2e tests

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -1,5 +1,5 @@
 import { AppPage } from './app.po';
-import { browser, logging } from 'protractor';
+import { browser, element, by, logging } from 'protractor';
 
 describe('workspace-project App', () => {
   let page: AppPage;
@@ -8,9 +8,40 @@ describe('workspace-project App', () => {
     page = new AppPage();
   });
 
-  it('should display welcome message', () => {
+  it('should have a Trasnfer Icon', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('peachtree-bank app is running!');
+    let xpath = element(by.xpath('//app-transfer/mat-card/mat-toolbar/mat-icon'));
+    expect(xpath.getText()).toBe('compare_arrows');
+  });
+
+  it('should have a FROM ACCOUNT label', () => {
+    page.navigateTo();
+    let xpath = element(by.xpath('//*[@id="mat-form-field-label-1"]/mat-label'));
+    expect(xpath.getText()).toBe('FROM ACCOUNT');
+  });
+
+  it('should have a TO ACCOUNT label', () => {
+    page.navigateTo();
+    let xpath = element(by.xpath('//*[@id="mat-form-field-label-3"]/mat-label'));
+    expect(xpath.getText()).toBe('TO ACCOUNT');
+  });
+
+  it('should have a AMOUNT ($) label', () => {
+    page.navigateTo();
+    let xpath = element(by.xpath('//*[@id="mat-form-field-label-5"]/mat-label'));
+    expect(xpath.getText()).toBe('AMOUNT ($)');
+  });
+
+  it('should have a Clear button', () => {
+    page.navigateTo();
+    let xpath = element(by.xpath('//mat-card-actions/button[1]/span'));
+    expect(xpath.getText()).toBe('Clear');
+  });
+
+  it('should have a Submit button', () => {
+    page.navigateTo();
+    let xpath = element(by.xpath('//mat-card-actions/button[2]/span'));
+    expect(xpath.getText()).toBe('Submit');
   });
 
   afterEach(async () => {
